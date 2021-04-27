0adf00a36a483c98ca5f93003d04292cebdddf67
Stepan <bai_777@mail.ru>
2021-04-27 20:45:00 +0300

    correct Solution4

--- src/main/java/Solution4.java
+++ src/main/java/Solution4.java
@@ -14,6 +14,27 @@ public class Solution4 {
             }
             System.out.println();
         }
+        print2dArray(makeDiagonalArray(6, 1));
+
     }
+
+    static int[][] makeDiagonalArray(int size, int value){
+        int[][] arr = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            arr[i][i] = value;
+            arr[i][arr.length - 1 - i] = value;
+        }
+        return arr;
+    }
+
+    static void print2dArray(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
 
